51bb156d47d0ca6feafa59dfe296a02388ad26d6
Reimo Kirmann <reimokirmann@gmail.com>
2020-05-28 22:27:26 +0300

    added deep sleep timeout and touch wakeup

--- BLE_uart/BLE_uart.ino
+++ BLE_uart/BLE_uart.ino
@@ -1,12 +1,15 @@
 /*
     Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Based on Neil Kolban example for IDF:
+   https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
     Ported to Arduino ESP32 by Evandro Copercini
 
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+   Create a BLE server that, once we receive a connection, will send periodic
+   notifications. The service advertises itself as:
+   6E400001-B5A3-F393-E0A9-E50E24DCCA9E Has a characteristic of:
+   6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send
+   data with  "NOTIFY"
 
    The design of creating the BLE server is:
    1. Create a BLE Server
@@ -16,122 +19,151 @@
    5. Start the service.
    6. Start advertising.
 
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second. 
+   In this example rxValue is the data received (only accessible inside that
+   function). And txValue is the data to be sent, in this example just a byte
+   incremented every second.
 */
+#include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
 
 BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
+BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue = 0;
+uint8_t pressed = 0;
+std::string previousRxValue = "";
+#define Threshold 40
+
+// timer
+const long timeout = 60000;  // interval at which to blink (milliseconds)
+unsigned long previousMillis = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define SERVICE_UUID \
+    "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"  // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) { deviceConnected = true; };
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
+    void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+        std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
-        
-        if (rxValue == "1"){
-        digitalWrite(2, HIGH);  
-        }
-        if (rxValue == "0"){
-        digitalWrite(2, LOW);
-        }
-        
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial.print(rxValue[i]);
+        if (rxValue.length() > 0) {
+            previousMillis = 0;
+            
+            if (rxValue == "1") {
+                digitalWrite(2, HIGH);
+            }
+            else if (rxValue == "0") {
+                digitalWrite(2, LOW);
+            }
 
-        Serial.println();
-        Serial.println("*********");
-      }
+			if (deviceConnected && rxValue != previousRxValue) {
+				previousRxValue = rxValue;
+                Serial.println("Sending change confirmation");
+                uint8_t confirm = 1;
+                pTxCharacteristic->setValue(&confirm, 1);
+                pTxCharacteristic->notify();
+            }
+
+            Serial.println("*********");
+            Serial.print("Received Value: ");
+            for (int i = 0; i < rxValue.length(); i++) Serial.print(rxValue[i]);
+
+            Serial.println();
+            Serial.println("*********");
+        }
     }
 };
 
-
+void callback(){
+  //placeholder callback function
+}
 void setup() {
-  Serial.begin(115200);
-pinMode(2, OUTPUT);
-  // Create the BLE Device
-  BLEDevice::init("UART Service");
+    Serial.begin(115200);
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // builtin led
+    pinMode(2, OUTPUT);
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+    // Setup interrupt on Touch Pad 0 (GPIO2)
+    touchAttachInterrupt(T0, callback, Threshold);
+    // Configure Touchpad as wakeup source
+    esp_sleep_enable_touchpad_wakeup();
 
-  // Create a BLE Characteristic
-  pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
-										BLECharacteristic::PROPERTY_NOTIFY
-									);
-                      
-  pTxCharacteristic->addDescriptor(new BLE2902());
+    // Create the BLE Device
+    BLEDevice::init("UART Service");
 
-  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-											 CHARACTERISTIC_UUID_RX,
-											BLECharacteristic::PROPERTY_WRITE
-										);
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Start the service
-  pService->start();
+    // Create a BLE Characteristic
+    pTxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
 
-  // Start advertising
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+    pTxCharacteristic->addDescriptor(new BLE2902());
+
+    BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
+
+    pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+    // Start the service
+    pService->start();
+
+    // Start advertising
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
+    previousMillis = millis();
 
-    if (deviceConnected) {
-      uint8_t btn = touchRead(2);
-      Serial.println(btn);
-        //pTxCharacteristic->setValue(&txValue, 1);
-        pTxCharacteristic->setValue(&btn, 1);
-        pTxCharacteristic->notify();
-        //txValue++;
-        
-		delay(1000); // bluetooth stack will go into congestion, if too many packets are sent
-	}
+    if (previousMillis >= timeout) {
+        // Go to sleep now
+        Serial.println("Going to sleep now");
+        esp_deep_sleep_start();
+    }
+
+    /*
+        if (deviceConnected) {
+          uint8_t btn = touchRead(T0);
+          btn > 40 ? pressed = 0 : pressed = 1;
+          //Serial.println(btn);
+            //pTxCharacteristic->setValue(&txValue, 1);
+            pTxCharacteristic->setValue(&pressed, 1);
+            pTxCharacteristic->notify();
+            //txValue++;
+
+                    delay(10); // bluetooth stack will go into congestion, if
+       too many packets are sent
+            }
+            */
 
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
+        delay(500);  // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising();  // restart advertising
         Serial.println("start advertising");
         oldDeviceConnected = deviceConnected;
     }
     // connecting
     if (deviceConnected && !oldDeviceConnected) {
-		// do stuff here on connecting
+        // do stuff here on connecting
         oldDeviceConnected = deviceConnected;
     }
 }
